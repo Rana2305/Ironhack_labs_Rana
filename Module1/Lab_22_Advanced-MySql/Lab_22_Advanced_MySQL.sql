8119c4f7060ed5c4aec51cfa9232f179c328a0eb
Rana2305 <80770351+Rana2305@users.noreply.github.com>
2022-03-31 23:59:48 +0200

    Add files via upload

---
+++ Module1/Lab_22_Advanced-MySql/Lab_22_Advanced_MySQL.sql
@@ -0,0 +1,51 @@
+use publications;
+########Challenge 1 - Most Profiting Authors########
+#### step 1 #####################################"""""
+
+select titleauthor.au_id, titles.title_id, (titles.advance * titleauthor.royaltyper/100) as total_advance, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty from titles
+JOIN titleauthor ON titles.title_id = titleauthor.title_id
+JOIN sales ON titles.title_id = sales.title_id
+group by titleauthor.au_id  ;
+
+#####step 2######################
+
+select new_table.au_id, new_table.title_id, 
+SUM(sales_royalty)  
+from  (select titleauthor.au_id, titles.title_id, (titles.advance * titleauthor.royaltyper/100) as total_advance, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty from titles
+JOIN titleauthor ON titles.title_id = titleauthor.title_id
+JOIN sales ON titles.title_id = sales.title_id group by titleauthor.au_id) as new_table 
+group by new_table.title_id ;
+
+##### step3#######
+select new_table.au_id, new_table.title_id, 
+(SUM(sales_royalty)+ SUM(total_advance)) as profit  
+from  (select titleauthor.au_id, titles.title_id, (titles.advance * titleauthor.royaltyper/100) as total_advance, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty from titles
+JOIN titleauthor ON titles.title_id = titleauthor.title_id
+JOIN sales ON titles.title_id = sales.title_id group by titleauthor.au_id) as new_table 
+group by new_table.title_id
+order by profit desc
+limit 3 ;
+
+############## Challenge 2 ##############
+########## step1##########
+CREATE TEMPORARY TABLE new_table2
+select titleauthor.au_id, titles.title_id, (titles.advance * titleauthor.royaltyper/100) as total_advance, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty from titles
+JOIN titleauthor ON titles.title_id = titleauthor.title_id
+JOIN sales ON titles.title_id = sales.title_id
+group by titleauthor.au_id  ;
+
+######step2 #############
+select new_table2.au_id, new_table2.title_id, 
+SUM(sales_royalty)  
+from  new_table2 
+group by new_table2.title_id ;
+
+#### step3 ######
+select new_table2.au_id, new_table2.title_id, 
+(SUM(sales_royalty)+ SUM(total_advance)) as profit  
+from  new_table2
+group by new_table2.title_id
+order by profit desc
+limit 3 ;
+
+
